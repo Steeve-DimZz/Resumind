68a8a585b7b0dffe51fc0f9d75925aed7797dc68
Steeve DimZz <steeve.dimzz@gmail.com>
2025-07-20 22:15:16 -0400

    Resume FeedBack

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -3,15 +3,23 @@
  * @param bytes - The file size in bytes
  * @returns A formatted string with the appropriate unit
  */
+
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 export function formatSize(bytes: number): string {
   if (bytes === 0) return '0 Bytes';
-  
+
   const k = 1024;
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  
+
   // Determine the appropriate unit
   const i = Math.floor(Math.log(bytes) / Math.log(k));
-  
+
   // Format the number with 2 decimal places
   return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
 }
